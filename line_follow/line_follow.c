ed8103ae66242905780b4562f12c2c8623ceeff0
LeoHL <leonid.hlieviy@gmail.com>
2026-05-19 11:26:30 +0200

    Added initial testing code for the LDR

---
+++ line_follow/line_follow.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "line_follow.h"
+
+float convert_adc(uint reading)
+{
+    return reading * ADC_CONVERT_VAL;
+}
+
+int main()
+{
+    adc_init();
+    adc_gpio_init(RIGHT_SENSOR);
+    adc_select_input(RIGHT_SENSOR_CH);
+    uint adc_raw;
+
+    while (true){
+        adc_raw = adc_read();
+        printf("%.2f\n", convert_adc(adc_raw));
+        sleep_ms(10);
+    }
+}
