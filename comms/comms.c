6610f80cc22c24ef87c5ff1ff54bc0fa40bf9b9d
Ouagadougou <132900761+whythrnonames@users.noreply.github.com>
2026-06-08 19:47:30 +0200

    Did a VERY big clean up of comms

--- comms/comms.c
+++ comms/comms.c
@@ -13,29 +13,15 @@ static void comms_callback(uint gpio, uint32_t events)
     uint32_t now = time_us_32();
 
     int ch = gpio - 6;
-/*
-    int ch = -1;
-    if (gpio == RECIEVER_PIN_CH1){
-        ch = 0;
-    } else if (gpio == RECIEVER_PIN_CH2){
-        ch = 1;
-    } else if (gpio == RECIEVER_PIN_CH3){
-        ch = 2;
-    } else if (gpio == RECIEVER_PIN_CH4){
-        ch = 3;
-    }
-*/
-    if (ch < 0 && ch > 4){
+
+    if (ch < 0)
         return;
-    }
     
 
     if (events & GPIO_IRQ_EDGE_RISE){
-        //gpio_acknowledge_irq(RECIEVER_PIN_CH1, GPIO_IRQ_EDGE_RISE);
         rise_time[ch] = now;
     } 
     if (events & GPIO_IRQ_EDGE_FALL){
-        //gpio_acknowledge_irq(RECIEVER_PIN_CH1, GPIO_IRQ_EDGE_FALL);
         pulse_width[ch] = time_us_32() - rise_time[ch];
     }
 }
@@ -53,14 +39,11 @@ void comms_init()
         gpio_set_dir(pin, GPIO_IN);
         gpio_pull_down(pin); 
 
-
     gpio_set_irq_enabled(pin,
         GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
         true);
    
     }
-    //printf("GPIO config done \n");
-    //printf("IRQ set up \n");
 }
 
 static float normalize(uint32_t pulse, float min_val, float max_val){
@@ -77,15 +60,8 @@ comms_output_state_t comms_read_CH()
     comms_state.ch2_output = normalize(pulse_width[1], 1000, 2000);
     comms_state.ch3_output = pulse_width[2] > 1500? 1 : 0;
     comms_state.ch4_output = pulse_width[3] > 1500? 1 : 0;
-/*
-    printf("CH1 output: %.2f CH2 output: %.2f CH3 output: %d CH4 output: %d \n", 
-        comms_state.ch1_output,
-        comms_state.ch2_output, 
-        comms_state.ch3_output, 
-        comms_state.ch4_output);
 
-*/
-    //sleep_ms(100);
 
+    sleep_ms(100);
     return comms_state;
 }
